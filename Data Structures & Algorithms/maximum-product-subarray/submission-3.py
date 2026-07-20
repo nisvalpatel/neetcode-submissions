8d1bb0f60890d548f5bcab8910acd3bc63892850
Nisval Patel <80929417+nisvalpatel@users.noreply.github.com>
2026-07-20 18:44:48 -0500

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        ret = nums[0]
+        minimum = 1
+        maximum = 1
+
+        for num in nums:
+            
+            # if there is a negative number
+            if num < 0:
+                temp = minimum
+                minimum = maximum
+                maximum = temp
+
+            minimum = min(num, num*minimum)
+            maximum = max(num, num*maximum)
+
+            ret = max(maximum, ret)
+
+        
+        return ret
+            
+
